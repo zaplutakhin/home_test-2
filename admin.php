e7a02dc66f7c624475772b9a560db949aa519687
Alexey Zaplutakhin <zaplutakhin@gmail.com>
2018-10-31 12:51:57 +0300

    исправления

--- admin.php
+++ admin.php
@@ -1,16 +1,16 @@
 <?php
   if (!empty($_FILES) && array_key_exists('test', $_FILES)) {
   $filename=date("G-i-s");
-  if (move_uploaded_file($_FILES['test']['tmp_name'], "./tests/$filename.json")) header('Location: http://university.netology.ru/user_data/azaplutakhin/php-http/list.php');
+  if (move_uploaded_file($_FILES['test']['tmp_name'], "./tests/$filename.json")) header('Location: list.php');
  }
  echo "<p><h3>Список тестов:</h3></p>";
  $dir = 'tests';
  $tests = scandir($dir);
  foreach ($tests as $test){
      if ($test !=='..' and $test!=='.' and $test!=='test.php'){
-        $testname=json_decode(file_get_contents('http://university.netology.ru/user_data/azaplutakhin/php-http/tests/'.urlencode($test)), true);
+        $testname=json_decode(file_get_contents('tests/'.urlencode($test)), true);
         $name=$testname['0']['testname'];
-        echo "<p><a href='php/tests/test.php?test=$test'> $name </a></p>";
+        echo "<p><a href='test.php?test=$test'> $name </a></p>";
  }
  }
 ?>
--- test.php
+++ test.php
@@ -1,18 +1,21 @@
 <?php 
+if (!$_GET) exit; 
+else{
+if (!@$test=file_get_contents('tests/'.$_GET["test"])) header('HTTP/1.1 404 Not Found'); else {
+
 echo "<p><b>Список тестов:</b></p>";
-$dir = '.';
-$tests = scandir($dir);
-foreach ($tests as $test){
-    if ($test !=='..' and $test!=='.' and $test!=='test.php'){
-       $testname=json_decode(file_get_contents('http://university.netology.ru/user_data/azaplutakhin/php-http/tests/'.urlencode($test)), true);
+$dir = 'tests';
+$tests_list = scandir($dir);
+foreach ($tests_list as $test_list){
+    if ($test_list !=='..' and $test_list!=='.'){
+       $testname=json_decode(file_get_contents('tests/'.urlencode($test_list)), true);
        $name=$testname['0']['testname'];
-       echo "<p><a href='test.php?test=$test'> $name </a></p>";
+       echo "<p><a href='test.php?test=$test_list'> $name </a></p>";
 }
 }
 echo '<hr>';
-if (!$_GET) exit; 
-else{
-$test=file_get_contents($_GET["test"]);
+
+$test=file_get_contents('tests/'.$_GET["test"]);
 $test=json_decode($test,true);
 $description=$test['0']['testname'];
 echo "<h3>$description:</h3>";
@@ -58,14 +61,14 @@ if (!empty($_POST)){
 } 
 
 $image = imagecreatetruecolor(300,212);
-$image_path = '../cert.jpg';
-$img = imagecreatefromjpeg($image_path);
+$image_path = 'cert.png';
+$img = imagecreatefrompng($image_path);
 $text_name = $_POST['name'];
 $i=$i-1;
 if ($mark==0 or $i >= 5) $text_mark = "$mark баллов из $i"; 
 if ($mark==1) $text_mark = "$mark балл из $i";
 if ($mark >= 2 and $i <= 4) $text_mark = "$mark балла из $i";
-$font = '../Helvetica.ttf';
+$font = 'Helvetica.ttf';
 $black = imagecolorallocate($image, 0, 0, 0);
 $textcolor = imagecolorallocate($image, 80, 80, 80);
 
@@ -79,10 +82,11 @@ imagettftext($image, 20, 0, $x_name, 110, $black, $font, $text_name);
 imagettftext($image, 16, 0, $x_mark, 150, $black, $font, $text_mark);
 imagepng($image, "sertificat.png");
 imagedestroy($image);
+
 ?>
 <img src="sertificat.png"></img>
 <?php
-
+}
 }
 }
 ?>
